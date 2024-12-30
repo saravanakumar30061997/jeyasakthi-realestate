b46767060631442e497769f4f03154c50f1ff9a1
Saravanakumar S <saravanask1997@gmail.com>
2024-12-30 20:31:07 +0530

    Initial commit

--- jeyasakthientr/settings.py
+++ jeyasakthientr/settings.py
@@ -134,10 +134,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
+# URL to access static files
+STATIC_URL = '/static/'
+
+# Directory to collect static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories (if any)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'jeyasakthientr/static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Media Folder Settings
